04c118aed0efed3746403b7fad9f1b426d262856
ericcug <eric.wong.cug@gmail.com>
2024-10-30 16:10:04 +0800

    fix fetch

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ class GoFileDownloader {
         }
 
         const headers = {
-            'User-Agent': 'Mozilla/5.0',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
             'Accept': '*/*',
             'Connection': 'keep-alive'
         };
@@ -162,7 +162,7 @@ class GoFileDownloader {
     async fetchJson(url) {
         return new Promise((resolve, reject) => {
             const headers = {
-                'User-Agent': 'Mozilla/5.0',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
                 'Accept': '*/*',
                 'Connection': 'keep-alive'
             };
